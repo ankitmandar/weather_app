bcde5c15052c97524008218086cdf4ad1abc7b90
Ankit Mandar <ankit.mandar.1@gmail.com>
2025-08-11 22:36:32 +0545

    Use of BackdropFilter and ClipRRect

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -12,25 +14,53 @@ class WeatherScreen extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {
-              print('Refresh');
-            },
-            icon: Icon(Icons.refresh),
-          ),
-        ],
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
       ),
-      body: Column(
-        spacing: 20,
-        children: [
-          // main card
-          Placeholder(fallbackHeight: 250),
-          // weather forecase card
-          Placeholder(fallbackHeight: 150),
-          // additional information card
-          Placeholder(fallbackHeight: 150),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          spacing: 20,
+          children: [
+            // main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadiusGeometry.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        spacing: 16,
+                        children: [
+                          Text(
+                            '300 °F',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(Icons.cloud, size: 64),
+                          Text('Rain', style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            // weather forecase card
+            Placeholder(fallbackHeight: 150),
+            // additional information card
+            Placeholder(fallbackHeight: 150),
+          ],
+        ),
       ),
     );
   }
